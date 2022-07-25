a5583ca4e3b257086fd188b73cf5efd8ccc52bcd
ishaannair <73781379+ishaannair@users.noreply.github.com>
2022-07-26 03:52:45 +0800

    added more animation regarding gun

--- game dev final project/Assets/Scripts/PlayerScript/SlashController.cs
+++ game dev final project/Assets/Scripts/PlayerScript/SlashController.cs
@@ -17,20 +17,21 @@ public class SlashController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(PlayerController.SlashActivated){
+        if(PlayerController.SlashActivated && PlayerController.playerWeapon==1){
             slashAnimator.Play("weapon_sword_side");
+            }
               // toggle state
       if (Input.GetKeyDown("a") && faceRightState){
-        Debug.Log("left");
+ 
           faceRightState = false;
           SwordSprite.flipX = false;
       }
 
       if (Input.GetKeyDown("d") && !faceRightState){
-        Debug.Log("right");
+
           faceRightState = true;
           SwordSprite.flipX = true;
       }
-    }
+    
     }
     }
